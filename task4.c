f4d776d703ca057a1fa99c09ef9adfbec6ca1735
OLEKSANDR NUZHYN <Nuzhyn.Alex@LLL.kpi.ua>
2025-04-28 13:00:11 +0300

    Create task4.c

---
+++ task4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Виконання whoami:\n");
+    system("whoami");
+    printf("\nВиконання id:\n");
+    system("id");
+  
+    return 0;
+}
