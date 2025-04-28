ac5183a2d7e1b4068144ac0d93afc002653b3175
OLEKSANDR NUZHYN <Nuzhyn.Alex@LLL.kpi.ua>
2025-04-28 14:05:26 +0300

    Create task7.sh

---
+++ task7.sh
@@ -0,0 +1,17 @@
+   #!/usr/local/bin/env bash
+   # command_logger.sh — вбудовуємо логування в bash через DEBUG-trap
+
+   # Куди писати лог
+   LOGFILE="$HOME/.user_commands.log"
+   
+   # Створимо/очистимо файл і дамо права тільки нам
+   : > "$LOGFILE"
+   chmod 600 "$LOGFILE"
+
+   # Кожну команду перед виконанням пишемо в лог (фіксується час, user, pwd і сама команда):
+   trap 'printf "%s %s %s %s\n" \
+     "$(date "+%Y-%m-%d %H:%M:%S")" "$USER" "$PWD" "$BASH_COMMAND" \
+     >> "$LOGFILE"' DEBUG
+
+   # У свій ~/.bashrc додаємо в кінець: source ~/task7.sh
+   # Виконуємо source ~/.bashrc
